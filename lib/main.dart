ed6129d0e765484081ebcfcb6ffbb9d718db1fcf
carlos <chlouzada@gmail.com>
2021-10-17 10:11:15 -0300

    feat: login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:nlw_payflow/modules/home/home_page.dart';
+import 'package:nlw_payflow/modules/login/login_page.dart';
 import 'package:nlw_payflow/modules/splash/splash_page.dart';
 import 'package:nlw_payflow/shared/themes/app_colors.dart';
 
@@ -14,9 +16,9 @@ class AppWidget extends StatelessWidget {
     return MaterialApp(
       title: 'Pay Flow',
       theme: ThemeData(
-        primaryColor: AppColors.primary
+        primaryColor: AppColors.primary,
       ),
-      home: const SplashPage(),
+      home: const LoginPage(),
     );
   }
 }
