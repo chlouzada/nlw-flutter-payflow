78fa79ce32c02b10134821a1a004908d5dd3eaca
carlos <chlouzada@gmail.com>
2021-10-17 22:23:59 -0300

    feat: fluxo de login com shared pref

--- lib/modules/login/login_controller.dart
+++ lib/modules/login/login_controller.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:nlw_payflow/shared/auth/auth_controller.dart';
+import 'package:nlw_payflow/shared/models/user_model.dart';
 
 class LoginController {
   final authController = AuthController();
@@ -12,10 +13,14 @@ class LoginController {
     );
     try {
       final response = await _googleSignIn.signIn();
-      authController.setUser(context,response);
+      final user = UserModel(
+        name: response!.displayName!,
+        photoUrl: response.photoUrl,
+      );
+      authController.setUser(context, user);
       // print(response);
     } catch (error) {
-      print("object");
+      authController.setUser(context, null);
       print(error);
     }
   }
